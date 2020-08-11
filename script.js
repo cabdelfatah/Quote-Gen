c58fbf6c7cf8eb4ad35269bd7213b303d17f217b
cabdelfatah <me.ab.delfatah@gmail.com>
2020-08-11 13:29:45 +0200

    new files created

---
+++ script.js
@@ -0,0 +1,63 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterbtn = document.getElementById('twitter');
+const newQuotebtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get Quote From API
+async function getQuote() {
+    loading();
+    const proxyUrl = 'https://murmuring-brook-65683.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json ();
+        // if Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        } 
+        // Reduce font size  for long quotes
+        if (data.quoteText.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long.quote');
+        }
+        // authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
+        // stop loader, show quote
+        complete();
+    }catch (error){
+        getQuote();
+    }
+}
+// tweet quote
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+twitterbtn.addEventListener('click', tweetQuote);
+newQuotebtn.addEventListener('click', getQuote);
+
+// On Load
+getQuote();
